8e007fb4e9996b22f38345b3f4e5fec58ebdd51e
mfrna <mfrna86@gmail.com>
2017-04-10 16:59:23 +0000

    Auto init embedded objects

--- Types/Message.php
+++ Types/Message.php
@@ -55,4 +55,27 @@ class Message extends Type
         'left_chat_member','new_chat_title','new_chat_photo','delete_chat_photo','group_chat_created',
         'supergroup_chat_created','channel_chat_created','migrate_to_chat_id','migrate_from_chat_id','pinned_message'
     ];
+
+    protected $objectTypes = [
+        'from' => User::class,
+        'chat' => Chat::class,
+        'forward_from' => User::class,
+        'forward_from_chat' => Chat::class,
+        'reply_to_message' => Message::class,
+        'entities' => [MessageEntity::class],
+        'audio' => Audio::class,
+        'document' => Document::class,
+        'game' => Game::class,
+        'photo' => [PhotoSize::class],
+        'sticker' => Sticker::class,
+        'video' => Video::class,
+        'voice' => Voice::class,
+        'contact' => Contact::class,
+        'location' => Location::class,
+        'venue' => Venue::class,
+        'new_chat_member' => User::class,
+        'left_chat_member' => User::class,
+        'new_chat_photo' => [PhotoSize::class],
+        'pinned_message' => Message::class
+    ];
 }
--- Types/Type.php
+++ Types/Type.php
@@ -10,10 +10,30 @@ abstract class Type implements \JsonSerializable{
     protected $props;
     protected $validProps;
     protected $readOnly;
+    protected $objectTypes;
 
-    public function __construct(array $response)
+    public function __construct(array $response, $resultOnly = false)
     {
-        $this->props = $response['result'];
+        if(!$resultOnly){
+            $response = $response['result'];
+        }
+        if(is_array($this->objectTypes) && count($this->objectTypes)){
+            foreach ($this->objectTypes as $objectType => $class){
+                // handle multi value resposnse, e.g. entities
+                if(!empty($response[$objectType]) && is_array($response[$objectType][0] )){
+                    foreach ($response[$objectType] as $subobjk => $subobjv){
+                        list($class) = $class;
+                        $response[$objectType][$subobjk] = new $class($subobjv, true);
+                    }
+                    // handle single value objects, e.g. user
+                }elseif(!empty($response[$objectType])){
+                    $response[$objectType] = new $class($response[$objectType], true);
+                }
+
+            }
+        }
+
+        $this->props = $response;
     }
 
 	public function __toString()
